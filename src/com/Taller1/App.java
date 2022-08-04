0b613c529f044fe2f69c727f3e844ba55ed0bfa2
Derek Sarmiento Loeber <dereksarmiento1@gmail.com>
2022-08-03 19:32:00 -0500

    Ejercicio 2 Resuelto

--- src/com/Taller1/App.java
+++ src/com/Taller1/App.java
@@ -2,6 +2,14 @@ package com.Taller1;
 
 public class App {
     public static void main(String[] args) {
+        int[] array = {4, 4, 5, 6, 7, 8, 10, 111111, 24};
+        int[] array2 = {4, 4, 5, 6, 7, 8, 10, 111111, 24};
 
+        System.out.println(Funciones.comparar(Funciones.crearArreglo(10),Funciones.crearArreglo(10)));
+        System.out.println(Funciones.comparar(array, array2));
+
+        System.out.println();
+        System.out.println(Funciones.comparacionRecursiva(Funciones.crearArreglo(10),Funciones.crearArreglo(10), 9));
+        System.out.println(Funciones.comparacionRecursiva(array, array2, 8));
     }
 }
--- src/com/Taller1/Funciones.java
+++ src/com/Taller1/Funciones.java
@@ -1,4 +1,31 @@
 package com.Taller1;
 
 public class Funciones {
+    static int[] crearArreglo(int size){
+        int[] array = new int[size];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)((Math.random() * (5000 - 0)) + 0);
+        }
+        return array;
+    }
+
+    static boolean comparar(int[] arreglo1, int[] arreglo2){
+        boolean a = true;
+        for(int i = 0; i < 3; i++){
+            if(arreglo1[i] != arreglo2[i]){
+                a = false;
+            }
+
+        }
+        return a;
+    }
+
+    static boolean comparacionRecursiva(int[] arreglo, int[] arreglo2, int i){
+        if(i < 0){
+            return true;
+        }else
+            return arreglo[i] == arreglo2[i] && comparacionRecursiva(arreglo,arreglo2, i - 1);
+    }
+
 }
